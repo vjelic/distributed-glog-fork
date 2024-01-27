d142301929058e5fbde9d4b786a6ebe6121481e7
Younseo Roh <younseo.roh@amd.com>
2024-01-26 19:19:01 -0600

    update docker build script

--- continuous_integration/rocmci/docker/build_docker.sh
+++ continuous_integration/rocmci/docker/build_docker.sh
@@ -7,7 +7,7 @@ set -e
 # pushd dask_src
 # git clone git@github.com:AMD-AI/RMM.git
 # git clone --recurse-submodules --branch cuda_array_interface_dev git@github.com:AMD-AI/cupy.git
-# git clone --branch rocm-add-rocml git@github.com:AMD-AI/distributed.git
+# git clone --branch dev-rocm git@github.com:ROCmSoftwarePlatform/distributed.git
 # git clone --branch rocm-dev-pyamdsmi-with-cupy git@github.com:AMD-AI/ucx-py.git
 # popd
 
@@ -28,6 +28,7 @@ CMAKE_VERSION=3.24.1
 UCX_COMMIT=v1.14.x
 
 podman build \
+    --security-opt label=disable \
     --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
     --build-arg ANACONDA_PYTHON_VERSION=${ANACONDA_PYTHON_VERSION} \
     --build-arg ROCM_VERSION=${ROCM_VERSION} \
